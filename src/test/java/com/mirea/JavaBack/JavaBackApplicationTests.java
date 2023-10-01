801526d4df76cac93df058697ac5ae1aa26493d4
Nikita <poberezhnyuk.n.i@edu.mirea.ru>
2023-10-01 15:06:18 +0300

    Initial commit

---
+++ src/test/java/com/mirea/JavaBack/JavaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirea.JavaBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
